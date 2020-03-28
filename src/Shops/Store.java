3f60a987667d7a98d4d2a595abd3da38b1b30bad
Abdul Samad <abdul.samad4068@gmail.com>
2020-03-28 22:21:37 +0500

    Updated Store Class

--- src/Shops/Store.java
+++ src/Shops/Store.java
@@ -1,32 +1,49 @@
 package Shops;
 
-//Complete
 import com.company.Item;
 
 import java.util.HashMap;
 
+
 abstract public class Store {
-    private final String name;
-    private final String address;
+    private String name;
+    private double register;
+    
     private HashMap<Item, Integer> inventory;
 
-    public Store(String name, String address, HashMap<Item, Integer> inventory) {
+    public Store(String name, double register, HashMap<Item, Integer> inventory) {
         this.name = name;
-        this.address = address;
+        this.register = register;
         this.inventory = inventory;
     }
 
+    public Store(String name, double register) {
+        this.name = name;
+        this.register = register;
+        this.inventory = new HashMap<>();
+    }
+
     public Store() {
-        this(null, null, new HashMap<>());
+        this(null,0,new HashMap<>());
+    }
+
+    public double getRegister() {
+        return register;
+    }
+
+    public void setRegister(double register) {
+        this.register = register;
+    }
+
+    public void setInventory(HashMap<Item, Integer> inventory) {
+        this.inventory = inventory;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getAddress() {
-        return address;
-    }
+    
 
     public HashMap<Item, Integer> getInventory() {
         return inventory;
@@ -39,12 +56,26 @@ abstract public class Store {
             inventory.put(item, quantity);
         }
     }
-
+   //change 
     public void addItem(Item item) {
-        this.addItem(item, 1);
+    	 if (inventory.containsKey(item)) 
+             System.out.println("Already present in the inventory");
+         else
+           	 this.addItem(item, 1);
+   
     }
 
     public void changeItemQuantity(Item item, int amount) {
         inventory.replace(item, inventory.get(item) + amount);
+        if (inventory.get(item) <= 0) inventory.remove(item);
+    }
+
+    @Override
+    public String toString() {
+        String str = "The store " + name + " has the following items:\n";
+        for(Item item : inventory.keySet()) {
+            str += item;
+        }
+        return str;
     }
 }
